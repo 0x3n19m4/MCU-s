7351fe47f2bb3442d8be73be307f0eb842b04b67
Tony Montana <129088575+0x3n19m4@users.noreply.github.com>
2025-09-23 12:50:56 +0300

    Create esp8266_server.ino

---
+++ esp8266_server.ino
@@ -0,0 +1,56 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <Servo.h>
+Servo servo1;
+
+ESP8266WebServer server(80);
+
+String html = "<h1>ESP Web Server</h1><p>Servo switch <a href=\"Servo_On\"><button>ON</button></a>&nbsp;<a href=\"Servo_Off\"><button>OFF</button></a></p>";
+const char* ssid = "PWNED";
+const char* password = "04091977";
+
+void setup(void) {
+  servo1.attach(2);
+  
+  Serial.begin(115200);
+
+  // WiFi Initialisation
+  WiFi.begin(ssid, password);
+
+  Serial.println("");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", [] () {
+    server.send(200, "text/html", html);
+  });
+
+  server.on("/Servo_On", [] () {
+    server.send(200, "text/html", html);
+    servo1.write(0);
+    Serial.println("Changing Servo state to ON");
+    delay(100);
+  });
+
+  server.on("/Servo_Off", [] () {
+    server.send(200, "text/html", html);
+    servo1.write(130);
+    Serial.println("Changing Servo state to OFF");
+    delay(100);
+  });
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
