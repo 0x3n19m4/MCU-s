2d90eb864062aa70e00260e3a2d1db5580bd8f2d
0x3n19m4 <129088575+0x3n19m4@users.noreply.github.com>
2025-10-10 15:40:38 +0300

    Update receiver.ino

--- receiver.ino
+++ receiver.ino
@@ -2,13 +2,22 @@ void receive()
 {
   if (Serial.available())
   {
-    String data = Serial.readStringUntil('\n');
-    Serial.print("Received: ");
-    Serial.println(data);
+    int data = Serial.parseInt();
+    if (data == 1)
+    {
+      digitalWrite(2, HIGH);
+      Serial.println("LED ON");
+    }
+    else
+    {
+      digitalWrite(2, LOW);
+      Serial.println("LED OFF");  
+    }
   }
 }
 
 void setup() {
+  pinMode(2, OUTPUT);
   Serial.begin(9600);
 }
 
