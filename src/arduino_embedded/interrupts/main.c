7d92e8bf7ef479f9e079d54ad531ad486157944c
0x3n19m4 <129088575+0x3n19m4@users.noreply.github.com>
2026-02-16 17:40:30 +0200

    Add timer interrupt functionality in main.c
    Implement timer interrupt to count ticks and toggle LED.

--- src/arduino_embedded/interrupts/main.c
+++ src/arduino_embedded/interrupts/main.c
@@ -5,6 +5,7 @@
 #include <util/delay.h>
 
 /* --- delay --- */
+/*
 int main(void)
 {
   DDRB |= (1 << DDB5) | (1 << DDB4);
@@ -15,6 +16,51 @@ int main(void)
   {
     PORTB ^= (1 << PORTB5);
     _delay_ms(1000);
+
+    if (!(PIND & (1 << PIND2)))
+      PORTB |= (1 << PORTB4);
+
+    if (!(PIND & (1 << PIND3)))
+      PORTB &= ~(1 << PORTB4);
+  }
+}
+*/
+
+volatile uint32_t ticks = 0;
+
+void timer_init(void)
+{
+  TCCR1B |= (1 << WGM12);
+  TCCR1B |= (1 << CS11) | (1 << CS10);
+
+  OCR1A = 249;
+  TIMSK1 |= (1 << OCIE1A);
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  ticks++;
+}
+
+/* -- interruption -- */
+int main(void)
+{
+  DDRB |= (1 << DDB5) | (1 << DDB4);
+  DDRD &= ~((1 << DDD2) | (1 << DDD3));
+  PORTD |= (1 << PORTD2) | (1 << PORTD3); // pull-up
+
+  timer_init();
+  sei();
+
+  uint32_t last_tick = 0;
+
+  while (1)
+  {
+    if (ticks - last_tick >= 1000)
+    {
+      PORTB ^= (1 << PORTB5);
+      last_tick = ticks;
+    }
 
     if (!(PIND & (1 << PIND2)))
       PORTB |= (1 << PORTB4);
